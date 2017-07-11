add2ae947aa7f60a215f6e5db21672d40028cdef
Christopher Ryder <cryder9898@gmail.com>
2017-07-11 07:44:00 -0400

    using jsonp

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function getQuote(e) {
      $.ajax({
        type: "GET",
        url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+       dataType: "jsonp",
        success: function(data) {
          var post = data.shift(); // The data is an array of posts. Grab the first one.
          author = post.title;
